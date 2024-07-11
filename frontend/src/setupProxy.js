0acd52458190dd7289ef8e18957abe1fa80e892c
Piyush Razdan <piyush.razdan@gmail.com>
2024-07-11 13:05:14 -0400

    Adding

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'https://wordle-production-5838.up.railway.app/',
-            changeOrigin: true,
-        })
-    );
+
+module.exports = function(app) {
+  app.use(
+    '/api', 
+    createProxyMiddleware({
+      target: 'https://wordle-production-5838.up.railway.app/', // Replace with your actual Railway backend URL
+      changeOrigin: true,
+      pathRewrite: {'^/api' : ''} // You can adjust or remove pathRewrite based on your backend routing
+    })
+  );
 };
