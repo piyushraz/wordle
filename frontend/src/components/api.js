54fa3c31ab7de3933d68e5f4dcc387148c8908ab
Piyush Razdan <piyush.razdan@gmail.com>
2024-07-11 13:12:29 -0400

    Adding

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,74 +1,56 @@
-function api_getUsername(cb){
-	let url="/api/username";
-	fetch(url, {
-		method: "GET", 
-		mode: "same-origin", 
-		cache: "no-cache", 
-		credentials: "same-origin", 
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		redirect: "follow", 
-		referrerPolicy: "no-referrer", 
-	})
-	.then(response=>response.json())
-	.then(data=>cb(data))
-	.catch(error=>console.log(error));
-}
+const backendUrl = 'https://wordle-production-5838.up.railway.app';
 
-function api_guess(username, guess, cb){
-	let url="/api/username/"+username+"/guess/"+guess;
-	fetch(url, {
-        method: "POST",
-        mode: "same-origin",
-        cache: "no-cache",
-        credentials: "same-origin",
+function api_getUsername(cb) {
+    let url = `${backendUrl}/api/username`;
+    fetch(url, {
+        method: "GET", 
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
         },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
     })
     .then(response => response.json())
     .then(data => cb(data))
-    .catch(error => console.log(error));
+    .catch(error => console.error('Error fetching username:', error));
 }
 
-function api_newgame(username, cb){
-    let url="/api/username/"+username+"/newgame";
+function api_guess(username, guess, cb) {
+    let url = `${backendUrl}/api/username/${username}/guess/${guess}`;
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ guess })
+    })
+    .then(response => response.json())
+    .then(data => cb(data))
+    .catch(error => console.error('Error making guess:', error));
+}
+
+function api_newgame(username, cb) {
+    let url = `${backendUrl}/api/username/${username}/newgame`;
     fetch(url, {
         method: "PUT",
-        mode: "same-origin",
-        cache: "no-cache",
-        credentials: "same-origin",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
         },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
     })
     .then(response => response.json())
     .then(data => cb(data))
-    .catch(error => console.log(error));
+    .catch(error => console.error('Error starting new game:', error));
 }
 
-  function api_getUserStats(username, cb) {
-    let url = "/api/username/"+username+"/stats";
+function api_getUserStats(username, cb) {
+    let url = `${backendUrl}/api/username/${username}/stats`;
     fetch(url, {
         method: "GET",
-        mode: "same-origin",
-        cache: "no-cache",
-        credentials: "same-origin",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
         },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
     })
     .then(response => response.json())
     .then(data => cb(data))
-    .catch(error => console.log(error));
+    .catch(error => console.error('Error fetching user stats:', error));
 }
 
-export { api_getUsername, api_guess, api_newgame, api_getUserStats }; 
-
+export { api_getUsername, api_guess, api_newgame, api_getUserStats };
